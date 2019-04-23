6fb7316fcc24226b9d81037e5a69312cc0c3efdc
jonasL96 <47625852+jonasL96@users.noreply.github.com>
2019-04-23 08:56:22 +0300

    Added picture related stuff

--- src/WebRTCChatApp/main.js
+++ src/WebRTCChatApp/main.js
@@ -23,4 +23,27 @@ function handleError(error) {
   console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
 }
 
+ function clearphoto() {
+    var context = canvas.getContext('2d');
+    context.fillStyle = "#AAA";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    var data = canvas.toDataURL('image/png');
+    photo.setAttribute('src', data);
+  }
+
+  function takepicture() {
+    var context = canvas.getContext('2d');
+    if (width && height) {
+      canvas.width = width;
+      canvas.height = height;
+      context.drawImage(video, 0, 0, width, height);
+    
+      var data = canvas.toDataURL('image/png');
+      photo.setAttribute('src', data);
+    } else {
+      clearphoto();
+    }
+  }
+
 navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
